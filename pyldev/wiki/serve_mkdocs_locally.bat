46403b63c4b58042ce08fc4818d7536512fecee3
Nayel Blidi <nayel.blidi@capgemini.com>
2024-12-09 10:05:44 +0100

    Add: Local and VPN mkdocs hosts

---
+++ pyldev/wiki/serve_mkdocs_locally.bat
@@ -0,0 +1,36 @@
+@echo off
+
+:: Converting the wiki to the mkdocs format
+echo 1/6 Converting raw markdown to mkdocs format...
+python raw_to_docs.py
+
+:: Install mkdocs and mkdocs-material if not already installed
+echo 2/6 Installing mkdocs and mkdocs-material...
+pip install mkdocs mkdocs-material --quiet >nul 2>&1
+
+:: Build the MkDocs site
+echo 3/6 Building the mkdocs site...
+mkdocs build >nul 2>&1
+
+:: Retrieve the local IP address
+echo 4/6 Retrieving local IP address...
+for /f "tokens=1" %%i in ('powershell -Command "(Get-NetIPAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ne 'Loopback Pseudo-Interface 1' }).IPAddress"') do set LOCAL_IP=%%i
+
+:: Check if the LOCAL_IP variable is set
+if "%LOCAL_IP%"=="" (
+    echo Failed to retrieve local IP address.
+    pause
+    exit /b 1
+)
+
+:: Serve the MkDocs site locally
+echo 5/6 Serving the mkdocs site on Local Host...
+start /B "" cmd /c "mkdocs serve >nul 2>&1"
+
+echo 6/6 Server is running, you may exit this terminal.
+
+:: Display the local IP address with color
+echo.
+powershell -Command "Write-Host 'MkDocs server up at http://127.0.0.1:8000/' -ForegroundColor Green"
+echo.
+
---
+++ pyldev/wiki/serve_mkdocs_locally.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Converting the wiki to the mkdocs format
+echo "1/6 Converting raw markdown to mkdocs format..."
+python raw_to_docs.py
+
+# Install mkdocs and mkdocs-material if not already installed
+echo "2/6 Installing mkdocs and mkdocs-material..."
+pip install mkdocs mkdocs-material --quiet
+
+# Build the MkDocs site
+echo "3/6 Building the mkdocs site..."
+mkdocs build
+
+# Retrieve the local IP address
+echo "4/6 Retrieving local IP address..."
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+# Check if the LOCAL_IP variable is set
+if [ -z "$LOCAL_IP" ]; then
+  echo "Failed to retrieve local IP address."
+  exit 1
+fi
+
+# Serve the MkDocs site locally
+echo "5/6 Serving the mkdocs site on Local Host..."
+nohup mkdocs serve /dev/null 2>&1 &
+
+# Pause to keep the script running
+echo "6/6 Server is running, you may press [Enter] key to exit."
+
+# Display the local IP address with color
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+read -p "\n\t MkDocs server up at ${GREEN}http://127.0.0.1:8000/${NC}"
